86899ac823bf32d3e9bcb2e8ad8351b3f3f3ae8f
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2024-04-17 07:37:44 +0000

    Braty-2

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,10 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
   
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+  
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
